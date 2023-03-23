9eb97c0eaa003c1a17601a815a17b99b23a6e94f
Harry Cooper <harry@bigbite.net>
2023-03-23 16:50:03 +0000

    style: phpcs changes

--- inc/views/release-notes-widget.php
+++ inc/views/release-notes-widget.php
@@ -1,16 +1,28 @@
 <?php
 	$version = get_post_meta( get_the_ID(), 'version', true );
-	$date = get_post_meta( get_the_ID(), 'release_date', true );
+	$date    = get_post_meta( get_the_ID(), 'release_date', true );
 ?>
 
 <article class="release-note-widget">
 	<header class="release-note-header">
 		<h1 class="release-note-title"><?php the_title(); ?></h1>
 		<div class="release-note-meta-wrapper">
-			<span class="release-note-meta release-note-date <?php if( empty( $date ) ) { echo 'empty'; } ?>"><?php echo esc_html( $date ); ?></span>
+			<span class="release-note-meta release-note-date
+			<?php
+			if ( empty( $date ) ) {
+				echo 'empty';
+			}
+			?>
+			"><?php echo esc_html( $date ); ?></span>
 		</div>
 		<div class="release-note-meta-wrapper">
-			<span class="release-note-meta release-note-version <?php if( empty( $version ) ) { echo 'empty'; } ?>"><?php echo esc_html( $version ); ?></span>
+			<span class="release-note-meta release-note-version
+			<?php
+			if ( empty( $version ) ) {
+				echo 'empty';
+			}
+			?>
+			"><?php echo esc_html( $version ); ?></span>
 		</div>
 	</header>
 	<div class="release-note-body">
