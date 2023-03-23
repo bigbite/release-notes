a9c19a942122f7f75948a86010ee78f37c354fd9
Harry Cooper <harry@bigbite.net>
2023-03-23 16:49:53 +0000

    chore: added comments to empty files

--- inc/gutenberg/blocks/markdown-parser/index.php
+++ inc/gutenberg/blocks/markdown-parser/index.php
@@ -1,8 +1,10 @@
 <?php
 
 if ( ! function_exists( 'release_notes_render_mardown_parser' ) ) {
+	/**
+	 * Markdown parser block renderer
+	 */
 	function release_notes_render_mardown_parser() {
-		return;
 	}
 }
 
@@ -12,9 +14,9 @@ if ( ! function_exists( 'release_notes_render_mardown_parser' ) ) {
 function register_release_notes_mardown_parser() {
 	register_block_type(
 		__DIR__,
-		array(
+		[
 			'render_callback' => 'release_notes_render_mardown_parser',
-		)
+		]
 	);
 }
 add_action( 'init', 'register_release_notes_mardown_parser' );
