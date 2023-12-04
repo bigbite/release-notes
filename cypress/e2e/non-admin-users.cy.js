d39175559edb4420987ab75923ba6afceb124759
John Munster <johnmunster@Johns-MBP.localdomain>
2023-12-04 12:46:39 +0000

    fix: refactored the test to loop over all users to run as one test

--- cypress/e2e/non-admin-users.cy.js
+++ cypress/e2e/non-admin-users.cy.js
@@ -1,13 +1,48 @@
 /// <reference types="cypress" />
 
-describe('non admin users should not be able to access release notes post type', () => {
-    beforeEach(() => {
-        cy.switchUser('Editor');
-        cy.visit("/wp-admin");
-    });
+const userRoles = [
+    {
+        'userRole': 'admin',
+        'isAllowed': true,
+    },
+    {
+        'userRole': 'editor',
+        'isAllowed': false,
+    },
+    {
+        'userRole': 'subscriber',
+        'isAllowed': false,
+    },
+    {
+        'userRole': 'contributor',
+        'isAllowed': false,
+    },
+    {
+        'userRole': 'author',
+        'isAllowed': false,
+    },
+]
 
-    it('should not allow non admin users to create release notes', () => {
-        cy.get('#menu-posts-release-note > .wp-has-submenu > .wp-menu-name').should('not.exist');
-        cy.get('#toplevel_page_release-notes > .wp-not-current-submenu > .wp-menu-name').should('exist');
+describe('non admin users should not be able to access release notes post type', () => {
+
+    userRoles.forEach((role) => {
+
+        describe(`${role.userRole} access`, () => {
+            beforeEach(() => {
+                cy.switchUser(role.userRole);
+                cy.visit('/admin/');
+            })
+
+            const roleLabel = role?.userRole ? role.userRole : role.userRole;
+            const assertion = role?.isAllowed ? 'exist' : 'not.exist';
+            const assertionLabel = role?.isAllowed ? 'shold' : 'should not';
+            
+        
+            it(`${roleLabel} ${assertionLabel} be able to create release notes`, () => {
+                cy.get('#menu-posts-release-note > .wp-has-submenu > .wp-menu-name').should(assertion);
+                cy.get('#toplevel_page_release-notes > .wp-not-current-submenu > .wp-menu-name').should('exist');
+            })
+        })
     })
+
 })
