2bcfcf33cf8b42efc4929b6320b2ced943dc999c
Gareth Elwell <gareth@bigbite.net>
2023-12-05 15:24:41 +0000

    test: insert content into release note seeder

--- cypress/seeds/ReleaseNoteSeeder.php
+++ cypress/seeds/ReleaseNoteSeeder.php
@@ -5,9 +5,92 @@ use WP_Cypress\Fixtures;
 
 class ReleaseNoteSeeder extends Seeder {
 	public function run() {
+
+		$release_note_template = '<!-- wp:heading -->
+				<h2 class="wp-block-heading">Overview</h2>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph -->
+				<p> %s </p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:heading -->
+				<h2 class="wp-block-heading">Added</h2>
+				<!-- /wp:heading -->
+
+				<!-- wp:list -->
+				<ul><!-- wp:list-item -->
+				<li> %s </li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li> %s </li>
+				<!-- /wp:list-item --></ul>
+				<!-- /wp:list -->
+
+				<!-- wp:heading -->
+				<h2 class="wp-block-heading">Changed</h2>
+				<!-- /wp:heading -->
+
+				<!-- wp:list -->
+				<ul><!-- wp:list-item -->
+				<li> %s </li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li> %s </li>
+				<!-- /wp:list-item --></ul>
+				<!-- /wp:list -->
+
+				<!-- wp:heading -->
+				<h2 class="wp-block-heading">Fixed</h2>
+				<!-- /wp:heading -->
+
+				<!-- wp:list -->
+				<ul><!-- wp:list-item -->
+				<li> %s </li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li> %s </li>
+				<!-- /wp:list-item --></ul>
+				<!-- /wp:list -->';
+
 		( new Fixtures\Post([
 			'post_type' => 'release-note',
-		]) )->create(10);
+			'post_content' => sprintf(
+				$release_note_template,
+				$this->faker->realText( 100 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->numerify( '#.#.#' )
+			),
+			'post_meta' => [
+				'version' => $this->faker->numerify( '#.#.#' ),
+			]
+		]))->create(1);
+
+		( new Fixtures\Post([
+			'post_type' => 'release-note',
+			'post_content' => sprintf(
+				$release_note_template,
+				$this->faker->realText( 100 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->realText( 40 ),
+				$this->faker->numerify( '#.#.#' )
+			),
+			'post_meta' => [
+				'version' => $this->faker->numerify( '#.#.#' ),
+			]
+		]))->create(1);
 	}
 }
 
