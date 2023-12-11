78b23e46d14ba67de0ff22fa222d2f744b917cba
John Munster <johnmunster@Johns-MBP.localdomain>
2023-12-11 16:30:30 +0000

    add: New test that varifies that is possible to delete release notes: q

---
+++ cypress/e2e/delete-release-note.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('release notes can be deleted', () => {
+    before(() => {
+        cy.visit('/wp-admin/edit.php?post_type=release-note');
+    });
+
+    it('should allow administrators to delete release notes', () => {
+        cy.get('#the-list').within(() => {
+           cy.get('#post-4 .row-actions').invoke('attr', 'style', 'position: static').find('.submitdelete').click();
+           cy.visit('/wp-admin/edit.php?post_type=release-note');
+            cy.get('tr').should('have.length', 1);
+        });
+        
+    });
+});
