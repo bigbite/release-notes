3eb8c6051d19efc362762bf0d35e826dab7cad98
John Munster <johnmunster@Johns-MBP.localdomain>
2023-12-12 14:43:12 +0000

    fix: adjusted the selector in the test to grab the most recent release note

--- cypress/e2e/delete-release-note.cy.js
+++ cypress/e2e/delete-release-note.cy.js
@@ -7,7 +7,7 @@ describe('release notes can be deleted', () => {
 
     it('should allow administrators to delete release notes', () => {
         cy.get('#the-list').within(() => {
-           cy.get('#post-4 .row-actions').invoke('attr', 'style', 'position: static').find('.submitdelete').click();
+           cy.get('#post-5 .row-actions').invoke('attr', 'style', 'position: static').find('.submitdelete').click();
            cy.visit('/wp-admin/edit.php?post_type=release-note');
             cy.get('tr').should('have.length', 1);
         });
