04eab162fa842d01f996c0b166a5457e5d6ebec2
John Munster <johnmunster@Johns-MacBook-Pro.local>
2023-12-13 09:49:32 +0000

    fix: added an extra assertion to the version in the top toolbar and changed the name of the file to be more specific to the current release note as the new assertion should only occur when the most recent release note is delete

--- cypress/e2e/delete-current-release-note.cy.js
+++ cypress/e2e/delete-current-release-note.cy.js
@@ -7,10 +7,10 @@ describe('release notes can be deleted', () => {
 
     it('should allow administrators to delete release notes', () => {
         cy.get('#the-list').within(() => {
-           cy.get('#post-5 .row-actions').invoke('attr', 'style', 'position: static').find('.submitdelete').click();
-           cy.visit('/wp-admin/edit.php?post_type=release-note');
+            cy.get('#post-5 .row-actions').invoke('attr', 'style', 'position: static').find('.submitdelete').click();
+            cy.visit('/wp-admin/edit.php?post_type=release-note');
             cy.get('tr').should('have.length', 1);
         });
-        
+        cy.get('#wp-admin-bar-release-note-version > [aria-haspopup="true"]').should('not.have.value', 'Version 1.6.0');
     });
 });
