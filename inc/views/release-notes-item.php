67151e0c04959c32ecfee29573ebeaeda6dedbb8
Jordan Harris <jordan@bigbite.net>
2024-11-01 16:32:17 +0000

    feat: Change layout and add translatable strings

--- inc/views/release-notes-item.php
+++ inc/views/release-notes-item.php
@@ -2,12 +2,14 @@
 	$version        = get_post_meta( get_the_ID(), 'version', true );
 	$date           = get_post_meta( get_the_ID(), 'release_date', true );
 	$is_pre_release = get_post_meta( get_the_ID(), 'is_pre_release', true );
+	$version_string = 'Version <span class="release-note-meta release-note-version ' . ( empty( $version ) ? 'empty' : '' ) . '">[' . esc_html( $version ) . ']</span>';
 ?>
 
-<article class="release-note-single<?php $is_pre_release && print ' is-pre-release'; ?>">
+<article class="release-note-single <?php $is_pre_release && print ' is-pre-release'; ?>">
 	<header class="release-note-header">
-		<h1 class="release-note-title"><?php the_title(); ?></h1>
+		<h1 class="release-note-title">[<?php the_title(); ?>] <?php echo ! empty( $version ) ? wp_kses_data( $version_string ) : ''; ?></h1>
 		<div class="release-note-meta-wrapper">
+			<p><?php _e( 'Release Date:', 'release-notes' ); ?></p>
 			<span class="release-note-meta release-note-date
 			<?php
 			if ( empty( $date ) ) {
@@ -15,15 +17,6 @@
 			}
 			?>
 			"><?php echo esc_html( $date ); ?></span>
-		</div>
-		<div class="release-note-meta-wrapper">
-			<span class="release-note-meta release-note-version
-			<?php
-			if ( empty( $version ) ) {
-				echo 'empty';
-			}
-			?>
-			"><?php echo esc_html( $version ); ?></span>
 		</div>
 	</header>
 	<div class="release-note-body">
--- inc/views/release-notes-widget.php
+++ inc/views/release-notes-widget.php
@@ -1,12 +1,14 @@
 <?php
-	$version = get_post_meta( get_the_ID(), 'version', true );
-	$date    = get_post_meta( get_the_ID(), 'release_date', true );
+	$version            = get_post_meta( get_the_ID(), 'version', true );
+	$date               = get_post_meta( get_the_ID(), 'release_date', true );
+		$version_string = 'Version <span class="release-note-meta release-note-version ' . ( empty( $version ) ? 'empty' : '' ) . '">[' . esc_html( $version ) . ']</span>'
 ?>
 
 <article class="release-note-widget">
-	<header class="release-note-header">
-		<h1 class="release-note-title"><?php the_title(); ?></h1>
+<header class="release-note-header">
+		<h1 class="release-note-title">[<?php the_title(); ?>] <?php echo ! empty( $version ) ? wp_kses_data( $version_string ) : ''; ?></h1>
 		<div class="release-note-meta-wrapper">
+			<p><?php _e( 'Release Date:', 'release-notes' ); ?></p>
 			<span class="release-note-meta release-note-date
 			<?php
 			if ( empty( $date ) ) {
@@ -15,15 +17,6 @@
 			?>
 			"><?php echo esc_html( $date ); ?></span>
 		</div>
-		<div class="release-note-meta-wrapper">
-			<span class="release-note-meta release-note-version
-			<?php
-			if ( empty( $version ) ) {
-				echo 'empty';
-			}
-			?>
-			"><?php echo esc_html( $version ); ?></span>
-		</div>
 	</header>
 	<div class="release-note-body">
 		<?php the_content(); ?>
@@ -31,6 +24,6 @@
 
 
 	<footer class="release-note-footer">
-		<a href="<?php echo esc_url( admin_url( 'admin.php?page=release-notes' ) ); ?>">View All Release Notes</a>
+		<a href="<?php echo esc_url( admin_url( 'admin.php?page=release-notes' ) ); ?>"><?php _e( 'View All Release Notes', 'release-notes' ); ?></a>
 	</footer>
 </article>
