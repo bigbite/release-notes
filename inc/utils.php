1dd6f32946ebefad5f087ca324f0c14a17c69be2
Jordan Harris <jordan@bigbite.net>
2024-11-15 14:06:57 +0000

    feat: add formatted date based on setting

--- inc/utils.php
+++ inc/utils.php
@@ -2,3 +2,16 @@
 /**
  * Used for utility functions.
  */
+
+/**
+ * Function to format the date correctly according to the date_format option.
+ *
+ * @param string $date - the current date.
+ */
+function get_formatted_date( $date ) {
+	$format   = get_option( 'date_format' );
+	$raw_date = new DateTime( $date );
+	$date     = $raw_date->format( $format );
+
+	return $date;
+}
--- inc/views/release-notes-widget.php
+++ inc/views/release-notes-widget.php
@@ -1,12 +1,12 @@
 <?php
 	$version        = get_post_meta( get_the_ID(), 'version', true );
 	$date           = get_post_meta( get_the_ID(), 'release_date', true );
-	$version_string = 'Version <span class="release-note-meta release-note-version ' . ( empty( $version ) ? 'empty' : '' ) . '">[' . esc_html( $version ) . ']</span>'
+	$version_string = '(version <span class="release-note-meta release-note-version ' . ( empty( $version ) ? 'empty' : '' ) . '">' . esc_html( $version ) . '</span>)';
 ?>
 
 <article class="release-note-widget">
 <header class="release-note-header">
-		<h1 class="release-note-title">[<?php the_title(); ?>] <?php echo ! empty( $version ) ? wp_kses_data( $version_string ) : ''; ?></h1>
+		<h1 class="release-note-title"><?php the_title(); ?> <?php echo ! empty( $version ) ? wp_kses_data( $version_string ) : ''; ?></h1>
 		<div class="release-note-meta-wrapper">
 			<p><?php _e( 'Release Date:', 'release-notes' ); ?></p>
 			<span class="release-note-meta release-note-date
@@ -15,7 +15,7 @@
 				echo 'empty';
 			}
 			?>
-			"><?php echo esc_html( $date ); ?></span>
+			"><?php echo esc_html( get_formatted_date( $date ) ); ?></span>
 		</div>
 	</header>
 	<div class="release-note-body">
